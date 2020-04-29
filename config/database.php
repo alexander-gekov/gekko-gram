13ba6acacf324a1d38add40bf3ba88a707948e93
Aleksandar Gekov <426537@student.fontys.nl>
2020-04-30 02:09:22 +0300

    added database_url

--- config/database.php
+++ config/database.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Str;
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 
 return [
 
@@ -15,7 +21,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'heroku_connection'),
 
     /*
     |--------------------------------------------------------------------------
@@ -90,6 +96,16 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
+        'heroku_connection' => array(
+            'driver' => 'mysql',
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+        ),
 
     ],
 
